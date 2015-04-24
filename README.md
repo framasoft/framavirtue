f036c34f93909960dddeefd3e3368b22f256acfc
JosephK <josephk@framasoft.org>
2015-04-24 17:28:51 +0200

    Framavirtue

---
+++ README.md
@@ -0,0 +1,9 @@
+Thème Wordpress enfant basé sur [Virtue](https://wordpress.org/themes/virtue)
+
+Principaux changements :
+
+  * L'essentiel est dans ``style.css``
+  * Barre de menu placé en bas du ``<header>`` dans ``templates/header.php``
+  * Ajout de la framanav dans ``base.php``
+  * Désactivation des Google Fonts dans ``themeoptions/options.php`` avec la ligne ``'disable_google_fonts_link' => true,`` 
+  * Ajout d'une class ``form-control`` sur les champs du formulaire de commentaire dans ``templates/comments.php``
---
+++ base.php
@@ -0,0 +1,27 @@
+<?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <script type="text/javascript" src="/nav/nav.js"></script>
+
+    <div id="wrapper" class="container">
+    <?php do_action('get_header');
+        get_template_part('templates/header');
+    ?>
+      <div class="wrap contentclass" role="document">
+
+          <?php include kadence_template_path(); ?>
+            
+          <?php if (kadence_display_sidebar()) : ?>
+            <aside class="<?php echo esc_attr(kadence_sidebar_class()); ?> kad-sidebar" role="complementary">
+              <div class="sidebar">
+                <?php include kadence_sidebar_path(); ?>
+              </div><!-- /.sidebar -->
+            </aside><!-- /aside -->
+          <?php endif; ?>
+          </div><!-- /.row-->
+        </div><!-- /.content -->
+      </div><!-- /.wrap -->
+      <?php do_action('get_footer');
+      get_template_part('templates/footer'); ?>
+    </div><!--Wrapper-->
+  </body>
+</html>
