3fd51719b9db6998dfe345e4c68f762a803c9e3f
Luc Didry <luc@framasoft.org>
2018-09-10 12:26:09 +0200

    Fix wp_register_style error

--- functions.php
+++ functions.php
@@ -1,3 +1,6 @@
 <?php
-wp_register_style( 'redux-google-fonts-virtue', '');
+function framavirtue_register_style() {
+    wp_register_style( 'redux-google-fonts-virtue', '');
+}
+add_action( 'wp_enqueue_scripts', 'framavirtue_register_style' );
  ?>
